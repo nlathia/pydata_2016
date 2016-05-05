d89d865aa59a01a1d40a0b7443b5f362e6670d03
nlathia <neal.lathia@cl.cam.ac.uk>
2016-05-05 21:34:26 +0100

    Add list of activities

---
+++ App/AccelerometerCollector/app/src/main/java/lathia/accelerometercollector/LabelAdapter.java
@@ -0,0 +1,67 @@
+package lathia.accelerometercollector;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LabelAdapter extends RecyclerView.Adapter<LabelAdapter.ViewHolder>
+{
+    public static class ViewHolder extends RecyclerView.ViewHolder
+    {
+        public TextView textView;
+
+        public ViewHolder(final TextView v)
+        {
+            super(v);
+            textView = v;
+        }
+    }
+
+    private final static String SELECTED = "Selected: ";
+
+    private final static String[] LABELS = {
+            "Standing",
+            "Walking",
+            "Running",
+            "Stairs",
+            "Train"
+    };
+
+    @Override
+    public LabelAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        final Context context = parent.getContext();
+        TextView v = (TextView) LayoutInflater.from(context).inflate(R.layout.text_field,
+                                                                     parent,
+                                                                     false);
+        v.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(final View view)
+            {
+                TextView textView = (TextView) view;
+                String activity = textView.getText().toString();
+                // Store label
+                Toast.makeText(context, SELECTED + activity, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position)
+    {
+        holder.textView.setText(LABELS[position]);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return LABELS.length;
+    }
+}
--- App/AccelerometerCollector/app/src/main/java/lathia/accelerometercollector/MainActivity.java
+++ App/AccelerometerCollector/app/src/main/java/lathia/accelerometercollector/MainActivity.java
@@ -1,14 +1,49 @@
 package lathia.accelerometercollector;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+    private Button sensingButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensingButton = (Button) findViewById(R.id.sensingButton);
+        setRecyclerView();
+        setButton();
+    }
+
+    private void setRecyclerView()
+    {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        if (recyclerView != null)
+        {
+            recyclerView.setHasFixedSize(true);
+
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(layoutManager);
+
+            LabelAdapter adapter = new LabelAdapter();
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+    private void setButton()
+    {
+        sensingButton.setText(R.string.sensing_start);
+    }
+
+    public void onSensingButtonClicked(final View view)
+    {
+        setButton();
     }
 }
